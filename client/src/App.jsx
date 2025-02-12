d538391ce1697c20e491be62fca6d27a3bfd2a2f
Shakila shaik <skshakilask67@gmail.com>
2025-02-12 11:09:47 +0530

    intialised the project with vite and tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <main className="bg-black min-h-screen">Blinkit-clone</main>;
+}
+
+export default App;
---
+++ client/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcssPostcss from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
+// Remove tailwindcss and autoprefixer from here
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcssPostcss(),
+        autoprefixer(),
+      ],
+    },
+  },
+});
