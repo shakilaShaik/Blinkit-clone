b6b0d39b5ba6e9ea59a9a39552de54b0dfa0349b
Shakila shaik <skshakilask67@gmail.com>
2025-02-21 18:14:45 +0530

    deleted client folder

--- client/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { RouterProvider } from "react-router-dom";
-import router from "./routes.jsx";
-import { createRoot } from "react-dom/client";
-createRoot(document.getElementById("root")).render(
-  // <StrictMode>
-
-  <RouterProvider router={router} />
-  // </StrictMode>
-);
