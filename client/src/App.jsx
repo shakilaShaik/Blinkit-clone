a9ae0347b856f1f6c122608509c83cdacf1c1c95
shakila <skshakilask67@gmail.com>
2025-02-23 20:34:56 +0530

    added Footer styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <main>
         <Outlet />
       </main>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const HomePage = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-blue-500">
-      <h1 className="text-4xl font-bold text-white">Welcome to Home Page</h1>
+    <div>
+      Home
     </div>
   );
 };
