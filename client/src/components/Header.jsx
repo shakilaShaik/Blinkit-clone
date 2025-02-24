b737859b4096644de2d1a110826fdf34a5b7f534
Shakila shaik <skshakilask67@gmail.com>
2025-02-24 15:36:14 +0530

    added type animation for search bar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,31 +1,31 @@
 import React from "react";
 import useMobile from "../hooks/useMobile";
 import logo from "../assets/logo.svg";
+import logo2 from "../assets/logo2.png";
 
 import Search from "./Search";
 const Header = () => {
   const [isMobile] = useMobile();
   const isSearchPage = location.pathname === "/search";
   return (
-    <header className="h-24 shadow-md ">
+    <header className="h-24 shadow-md sticky top-0">
       {/* {!(isSearchPage && isMobile) && (
         <div className="container mx-auto flex items-center px-2  justify-between"></div>
       )}
       <div className="hidden lg:block">
         <Search />
-
-
-
       </div> */}
       {/* logo */}
-      <div className=" flex container mx-auto h-full items-center justify-between">
+      <div className=" flex container mx-auto h-full items-center justify-between px-4">
         <div className="h-full">
           <div className="h-full flex justify-center items-center">
-            <img src={logo} width={60} height={70} alt="logo" />
+            <img src={logo2} width={160} height={70} alt="logo" />
           </div>
         </div>
 
-        <div>Search</div>
+        <div>
+          <Search />
+        </div>
 
         <div> Login</div>
       </div>
