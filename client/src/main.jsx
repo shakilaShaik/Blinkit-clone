f590aac88bcb83204e9b4016afd92834bf13154d
Shakila shaik <skshakilask67@gmail.com>
2025-02-24 11:59:56 +0530

    setup tailwind correctly

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
