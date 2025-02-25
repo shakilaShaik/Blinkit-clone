3a3ba41a7fe94003e04003c83caed578cefaa814
Shakila shaik <skshakilask67@gmail.com>
2025-02-25 14:58:42 +0530

    implemented the logo, search, login for desktop and mobile

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,33 +1,59 @@
-import React from "react";
-import useMobile from "../hooks/useMobile";
-import logo from "../assets/logo.svg";
-import logo2 from "../assets/logo2.png";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import logo from "../assets/logo2.png";
 import Search from "./Search";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { FaRegCircleUser } from "react-icons/fa6";
+import useMobile from "../hooks/useMobile";
+import { BsCart4 } from "react-icons/bs";
 const Header = () => {
   const [isMobile] = useMobile();
+  const location = useLocation();
   const isSearchPage = location.pathname === "/search";
+  const navigate = useNavigate();
+
   return (
-    <header className="h-24 shadow-md sticky top-0">
-      {/* {!(isSearchPage && isMobile) && (
-        <div className="container mx-auto flex items-center px-2  justify-between"></div>
+    <header className="h-24 lg:h-20 lg:shadow-md sticky top-0 z-40 flex flex-col justify-center gap-1 bg-white">
+      {!(isSearchPage && isMobile) && (
+        <div className="container mx-auto flex items-center px-2 justify-between">
+          {/**logo */}
+          <div className="h-full">
+            <Link to={"/"} className="h-full flex justify-center items-center">
+              <img
+                src={logo}
+                width={170}
+                height={60}
+                alt="logo"
+                className="hidden lg:block"
+              />
+              <img
+                src={logo}
+                width={120}
+                height={60}
+                alt="logo"
+                className="lg:hidden"
+              />
+            </Link>
+          </div>
+
+          {/**Search */}
+          <div className="hidden lg:block">
+            <Search />
+          </div>
+
+          {/**login and my cart */}
+          <div className="">
+            {/**user icons display in only mobile version**/}
+            <button
+              className="text-neutral-600 lg:hidden"
+              // onClick={handleMobileUser}
+            >
+              <FaRegCircleUser size={26} />
+            </button>
+          </div>
+        </div>
       )}
-      <div className="hidden lg:block">
+      <div className="container mx-auto px-2 lg:hidden">
         <Search />
-      </div> */}
-      {/* logo */}
-      <div className=" flex container mx-auto h-full items-center justify-between px-4">
-        <div className="h-full">
-          <Link to={"/"} className="h-full flex justify-center items-center">
-            <img src={logo2} width={160} height={70} alt="logo" />
-          </Link>
-        </div>
-
-        <div className="container mx-auto flex items-center px-2 justify-center">
-          <Search />
-        </div>
-
-        <div> Login</div>
       </div>
     </header>
   );
