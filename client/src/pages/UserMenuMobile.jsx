3616c1cd6483720ef19be74c3e51482c768598b3
Shakila shaik <skshakilask67@gmail.com>
2025-03-06 14:54:11 +0530

    implementing the user-menu-mobile ui

--- client/src/pages/UserMenuMobile.jsx
+++ client/src/pages/UserMenuMobile.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import UserMenu from "../components/UserMenu";
+import { IoClose } from "react-icons/io5";
 
 export const UserMenuMobile = () => {
   return (
     <section className="bg-white h-full w-full py-8">
-      <div className="container mx-auto p-3">
+      <button
+        onClick={() => window.history.back()}
+        className="text-neutral-800 block w-fit ml-auto py-2 px-3"
+      >
+        <IoClose size={24} />
+      </button>
+
+      <div className="container mx-auto px-3 py-5 pb-8">
         <UserMenu />
       </div>
     </section>
