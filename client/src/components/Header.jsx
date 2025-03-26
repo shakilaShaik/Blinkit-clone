3e354f0e6e3fe052a8de66a25d4903148c1b460e
Shakila shaik <skshakilask67@gmail.com>
2025-03-26 12:46:24 +0530

    fixed header styling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -42,22 +42,8 @@ const Header = () => {
     navigate("/user");
   };
 
-  //total item and total price
-  // useEffect(()=>{
-  //     const qty = cartItem.reduce((preve,curr)=>{
-  //         return preve + curr.quantity
-  //     },0)
-  //     setTotalQty(qty)
-
-  //     const tPrice = cartItem.reduce((preve,curr)=>{
-  //         return preve + (curr.productId.price * curr.quantity)
-  //     },0)
-  //     setTotalPrice(tPrice)
-
-  // },[cartItem])
-
   return (
-    <header className="h-24 lg:h-20 lg:shadow-md sticky top-0 z-40 flex flex-col justify-center gap-1 bg-white">
+    <header className="h-22 lg:h-20 lg:shadow-md sticky top-0 z-40 flex flex-col justify-center gap-1 bg-white">
       {!(isSearchPage && isMobile) && (
         <div className="container mx-auto flex items-center px-2 justify-between">
           {/**logo */}
@@ -147,7 +133,7 @@ const Header = () => {
         </div>
       )}
 
-      <div className="container mx-auto px-2 lg:hidden">
+      <div className="container mx-auto px-2 lg:hidden ">
         <Search />
       </div>
 
